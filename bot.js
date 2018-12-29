afed5ea21df74e4831ea4f93f74a143346363f59
ahmad107996 <36535354+ahmad107996@users.noreply.github.com>
2018-12-29 21:55:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,6 +22,21 @@ if (!channel) return;
 channel.send(`<@${member.user.id}> *Welcome To Server Family Royal King...!*`)
 }}); 
 
+client4.on('ready',async () => {//Toxic Codes
+  console.log("Starting..");//Toxic Codes
+  let g = client4.guilds.get("473244584781021195"); // id server
+  let c = g.channels.get("473251118034714624");// id channel
+  if(c.type === 'voice') {//Toxic Codes
+  c.join();//Toxic Codes
+  setInterval(() => {//Toxic Codes
+  if(!g.me.voiceChannel) c.join();
+  }, 1);//Toxic Codes
+  } else {//Toxic Codes
+  console.log("Failed To Join:\n The Channel Type isn't \"text\"");
+  }//Toxic Codes
+  });
+
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
 client2.login(process.env.BOT_TOKEN2);// لا تغير فيها شيء
-client3.login(process.env.BOT_TOKEN3);// لا تغير فيها شيء
+client3.login(process.env.BOT_TOKEN3);// لا تغير فيها 
+  client4.login(process.env.BOT_TOKEN4);// لا تغير فيها 
