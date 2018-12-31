90747948885be53efa63e4b71ae706a17134cba7
ahmad107996 <36535354+ahmad107996@users.noreply.github.com>
2018-12-31 22:49:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -42,14 +42,14 @@ client4.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!devs.includes(message.author.id)) return;
       
-  if (message.content.startsWith('(prefix)ply')) {
+  if (message.content.startsWith('.ply')) {
     client4.user.setGame(argresult);
       message.channel.send(`**✅   ${argresult}**`)
   } else 
      if (message.content === ("leave")) {
     message.guild.leave();        
   } else  
-  if (message.content.startsWith('(prefix)wt')) {
+  if (message.content.startsWith('.wt')) {
   client4.user.setActivity(argresult, {type:'WATCHING'});
       message.channel.send(`**✅   ${argresult}**`)
   } else 
@@ -57,15 +57,15 @@ client4.on('message', message => {
   client4.user.setActivity(argresult , {type:'LISTENING'});
       message.channel.send(`**✅   ${argresult}**`)
   } else 
-  if (message.content.startsWith('(prefix)st')) {
+  if (message.content.startsWith('.st')) {
     client4.user.setGame(argresult, "https://www.twitch.tv/idk");
       message.channel.send(`**✅**`)
   }
-  if (message.content.startsWith('(prefix)setname')) {
+  if (message.content.startsWith('.setname')) {
   client4.user.setUsername(argresult).then
       message.channel.send(`Changing The Name To ..**${argresult}** `)
 } else
-if (message.content.startsWith('(prefix)setavatar')) {
+if (message.content.startsWith('.setavatar')) {
   client4.user.setAvatar(argresult);
     message.channel.send(`Changing The Avatar To :**${argresult}** `);
 }
