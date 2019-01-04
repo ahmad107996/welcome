3fcfdba875012cdf698bf76e1570321f17b1fe3d
ahmad107996 <36535354+ahmad107996@users.noreply.github.com>
2019-01-04 22:35:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const client2 = new Discord.Client();
-const client3 = new Discord.Client();
+
 
 client.on("guildMemberAdd", member => {
   if(member.guild.id === "274665615833432064") {  
@@ -9,18 +8,7 @@ const channel = member.guild.channels.find('id', '450468402650284033');
 if (!channel) return;
 channel.send(`<@${member.user.id}> *Welcome To Server Family Royal King...!*`)
 }}); 
-client2.on("guildMemberAdd", member => {
-  if(member.guild.id === "274665615833432064") {  
-const channel = member.guild.channels.find('id', '450468402650284033'); 
-if (!channel) return;
-channel.send(`<@${member.user.id}> *Welcome To Server Family Royal King...!*`)
-}}); 
-client3.on("guildMemberAdd", member => {
-  if(member.guild.id === "274665615833432064") {  
-const channel = member.guild.channels.find('id', '450468402650284033'); 
-if (!channel) return;
-channel.send(`<@${member.user.id}> *Welcome To Server Family Royal King...!*`)
-}}); 
+ 
 
 const client4 = new Discord.Client();
 
@@ -86,6 +74,4 @@ client4.on('ready', () => {
     });
 
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
-client2.login(process.env.BOT_TOKEN2);// لا تغير فيها شيء
-client3.login(process.env.BOT_TOKEN3);// لا تغير فيها 
 client4.login(process.env.BOT_TOKEN4);// لا تغير فيها 
