eae08010eaef4b5c79ac201d2566fc48a1002b8d
ahmad107996 <36535354+ahmad107996@users.noreply.github.com>
2019-02-22 11:38:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,10 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const client4 = new Discord.Client();
 
+client.on('ready' , async =>{
+console.log(`'${client.user.username}' on`)
+console.log(`'${client4.user.username}' on`)
+});
 
 client4.on("guildMemberAdd", member => {
   if(member.guild.id === "274665615833432064") {  
@@ -9,22 +13,12 @@ const channel = member.guild.channels.find('id', '541691032060821504');
 if (!channel) return;
 channel.send(`<@${member.user.id}> **Welcome to Royal Family <:RF:542067695517171733> **`)
 }}); 
- 
+ clinet.on('ready' , ayns
 
-
-
-client4.on('ready',  () => {
-  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
-  console.log('by BadGuY');
-  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-  console.log(`Logged in as  * [ " ${client4.user.username} " ] servers! [ " ${client.guilds.size} " ]`);
-  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-  console.log('is online')
-});
-
-const devs = ["348953140315291649"];
-const adminprefix = ["ahmad-"];
 client4.on('message', message => {
+  var devs = ["348953140315291649"];
+var adminprefix = ["ahmad-"];
+
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!devs.includes(message.author.id)) return;
       
@@ -56,6 +50,44 @@ if (message.content.startsWith(adminprefix + 'avatar')) {
     message.channel.send(`Changing The Avatar To :**${argresult}** `);
 }
 }); 
+
+client.on('message', message => {
+  var devs = ["462434603895095297"];
+var adminprefix = ["s-"];
+
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+      
+if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+if (message.content.startsWith(adminprefix + 'leave')) {
+    message.guild.leave();        
+  } else  
+if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+if (message.content.startsWith(adminprefix + 'mils')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+      message.channel.send(`**✅**`)
+  }
+if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith(adminprefix + 'avatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
+}); 
+
+
 
 
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
