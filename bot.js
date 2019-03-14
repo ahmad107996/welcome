dd202918eb3bcc707de2f1225c24dd28dcc39532
ahmad107996 <36535354+ahmad107996@users.noreply.github.com>
2019-03-14 21:32:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ client4.on('message', msg => {
         if (msg.member.voiceChannel) {
 
          if (msg.member.voiceChannel.joinable) {
-         msg.member.voiceChannel.join().then(msg.react(👍'));
+         msg.member.voiceChannel.join().then(msg.react('👍'));
      }
     }
 }
